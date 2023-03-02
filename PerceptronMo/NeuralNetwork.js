1eca85f15ce82157f339e50e857ea566979afd3c
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-03-02 18:24:03 +0100

    update

--- PerceptronMo/NeuralNetwork.js
+++ PerceptronMo/NeuralNetwork.js
@@ -3,18 +3,18 @@ class NeuralNetwork {
     learnrate = 0.01;
     trainIndex = 0;
 
-    NeuralNetwork(RandomHeightMax, RandomWidthMax) {
-        this.w1 = random(-1, 1);
-        this.w2 = random(-1, 1);
+    constructor(RandomHeightMax, RandomWidthMax) {
+        w1 = random(-1, 1);
+        w2 = random(-1, 1);
         for (let i = 0; i < 100; i++) {
-            this.points.push(
+            points.push(
                 new Point(random(RandomHeightMax), random(RandomWidthMax))
             );
         }
     }
     guess(x, y) {
-        let value = this.w1 * x + this.w2 * y;
-        if (n >= 0) {
+        let value = w1 * x + w2 * y;
+        if (value >= 0) {
             return 1;
         } else {
             return -1;
@@ -22,44 +22,45 @@ class NeuralNetwork {
     }
 
     trainAllNeurons() {
-        for (let i = 0; i < this.points.length; i++) {
-            let x = this.points[i].x;
-            let y = this.points[i].y;
+        for (let i = 0; i < points.length; i++) {
+            let x = points[i].x;
+            let y = points[i].y;
 
-            let label = this.points[i].label;
+            let label = points[i].label;
             let guess = guess(x, y);
             let error = label - guess;
 
-            this.w1 += error * x * this.learnrate;
-            this.w2 += error * y * this.learnrate;
+            w1 += error * x * learnrate;
+            w2 += error * y * learnrate;
         }
     }
     trainSingleNeuron() {
-        let x = this.points[this.trainIndex].x;
-        let y = this.points[this.trainIndex].y;
+        console.log("training");
+        let x = points[trainIndex].x;
+        let y = points[trainIndex].y;
 
-        let label = this.points[this.trainIndex].label;
+        let label = points[trainIndex].label;
         let guess = guess(x, y);
 
         let error = label - guess;
-        this.w1 += error * x * this.learnrate;
-        this.w2 += error * y * this.learnrate;
+        w1 += error * x * learnrate;
+        w2 += error * y * learnrate;
 
-        this.trainIndex++;
-        if (this.trainIndex == this.points.length) {
-            this.trainIndex = 0;
+        trainIndex++;
+        if (trainIndex == points.length) {
+            trainIndex = 0;
         }
     }
     show() {
         push();
-        for (let i = 0; i < this.points.length; i++) {
-            stroke(0);
-            if (this.points[i].label == 1) {
+        stroke(0);
+        for (let i = 0; i < points.length; i++) {
+            if (points[i].label == 1) {
                 fill(0, 0, 255);
             } else {
                 fill(0, 255, 0);
             }
-            ellipse(this.points[i].x, this.points[i].y, 8, 8);
+            ellipse(points[i].x, points[i].y, 8, 8);
         }
         pop();
     }
--- PerceptronMo/sketch.js
+++ PerceptronMo/sketch.js
@@ -1,7 +1,18 @@
+let pointCreationHeight;
+let pointCreationWidth;
+let brain;
+
 function setup() {
     createCanvas(400, 400);
+    pointCreationHeight = height - 50;
+    pointCreationWidth = width;
+    brain = new NeuralNetwork(pointCreationHeight, pointCreationWidth);
 }
 
 function draw() {
     background(155);
+    brain.show();
+}
+function mousePressed() {
+    brain.trainAllNeurons();
 }
