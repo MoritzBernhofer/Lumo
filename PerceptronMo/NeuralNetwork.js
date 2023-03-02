b464e883d6494f15f5bf6899bcdfe705541cd0dc
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-03-02 13:24:52 +0100

    huge update

---
+++ PerceptronMo/NeuralNetwork.js
@@ -0,0 +1,60 @@
+class NeuralNetwork {
+    points = [];
+
+    NeuralNetwork(RandomHeightMax, RandomWidthMax) {
+        this.w1 = random(-1, 1);
+        this.w2 = random(-1, 1);
+        for (let i = 0; i < 100; i++) {
+            this.points.push(
+                new Point(random(RandomHeightMax), random(RandomWidthMax))
+            );
+        }
+    }
+    guess(x, y) {
+        return this.sign(this.w1 * x + this.w2 * y);
+    }
+    sign(n) {
+        if (n >= 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    trainAllNeurons() {
+        for (let i = 0; i < this.points.length; i++) {
+            let x = this.points[i].x;
+            let y = this.points[i].y;
+            let label = this.points[i].label;
+            let guess = guess(this.points[i].x, this.points[i].y);
+            let error = label - guess;
+            this.w1 += error * x * 0.01;
+            this.w2 += error * y * 0.01;
+        }
+    }
+    trainSingleNeuron() {}
+    show() {
+        push();
+        for (let i = 0; i < this.points.length; i++) {
+            stroke(0);
+            if (this.points[i].label == 1) {
+                fill(0, 0, 255);
+            } else {
+                fill(0, 255, 0);
+            }
+            ellipse(this.points[i].x, this.points[i].y, 8, 8);
+        }
+        pop();
+    }
+}
+class Point {
+    Point(x, y) {
+        this.x = x;
+        this.y = y;
+        if (x > y) {
+            this.label = 1;
+        } else {
+            this.label = -1;
+        }
+    }
+}
