4d7d11c15217207be54abdca47850c8824bf7407
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-03-14 11:06:16 +0100

    update

--- NumberRecognition/NumberRecognition/Program.cs
+++ NumberRecognition/NumberRecognition/Program.cs
@@ -1,12 +1,11 @@
-﻿using NumberRecognition.Image;
-using System;
-using System.IO;
-namespace ReadDataFromByte;
+﻿using NumberRecognition.ImageFolder;
+
+namespace NumberRecognition;
 class ZuWild {
     static void Main(string[] args) {
         Image[] images = ImageHelper.LoadImages("../../../Images Mnist.bytes", "../../../Labels Mnist.bytes");
-        Console.WriteLine(images.Length);
-        Console.ReadKey();
+        //53k images
+
     }
 
 }
