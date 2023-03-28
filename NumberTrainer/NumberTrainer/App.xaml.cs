66852fb017c313e1324316ae08dc46767465b5db
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-03-28 14:05:58 +0200

    update

---
+++ NumberTrainer/NumberTrainer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NumberTrainer;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NumberTrainer/NumberTrainer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NumberTrainer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
