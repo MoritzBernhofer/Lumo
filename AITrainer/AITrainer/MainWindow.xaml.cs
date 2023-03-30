285ead9c40f6ac7f3a64287a13445acb3bb3b211
Moritz Bernhofer <114606708+MoritzBernhofer@users.noreply.github.com>
2023-03-30 07:59:28 +0200

    update

--- AITrainer/AITrainer/MainWindow.xaml.cs
+++ AITrainer/AITrainer/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,8 +21,38 @@ namespace AITrainer {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private Progress progress;
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Minimize_Window(object sender, RoutedEventArgs e) {
+            MainTab.WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Window(object sender, RoutedEventArgs e) {
+            MainTab.Close();
+        }
+        private void Maximize_Window(object sender, RoutedEventArgs e) {
+            if (MainTab.WindowState == WindowState.Maximized)
+                MainTab.WindowState = WindowState.Normal;
+            else
+                MainTab.WindowState = WindowState.Maximized;
+        }
+
+        private void ProgressThing(object sender, RoutedEventArgs e) {
+            progress = new(new Vector2((int)Math.Round(Progress_Display.Width), (int)Math.Round(Progress_Display.Height)));
+            progress.Update(30);
+            bool[,] array = progress.GetProgress();
+
+            Bitmap newImage = new(array.GetLength(0), array.GetLength(1));
+
+
         }
     }
 }
---
+++ AITrainer/AITrainer/Progress.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace AITrainer {
+    public class Progress {
+        private bool[,] image;
+        private int currentProgress = 0;
+        private Vector2 dimensions;
+        private Random rd = new();
+
+        public Progress(Vector2 dimensions) {
+            this.dimensions.X = (int)Math.Round(dimensions.X);
+            this.dimensions.Y = (int)Math.Round(dimensions.Y);
+            image = new bool[(int)this.dimensions.X, (int)this.dimensions.Y];
+
+        }
+        public void Update(int progressInPercent) {
+            if (progressInPercent < currentProgress) return;
+
+            int addProgress = currentProgress - progressInPercent;
+            int addPixel = (int)Math.Round(dimensions.X * dimensions.Y) * (addProgress / 100);
+
+            while(addPixel > 0 ) {
+                int x = rd.Next(0, (int)dimensions.X + 1);
+                int y = rd.Next(0, (int)dimensions.Y + 1);
+
+                if (image[x, y] == false) {
+                    image[x, y] = true;
+                    addPixel--;
+                }
+            }
+        }
+        public bool[,] GetProgress() => image;
+    }
+}
